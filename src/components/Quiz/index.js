0832f4ddabd353a9925139845da8e26586de809b
Daniel Escobar <escobard90@gmail.com>
2018-01-18 16:56:24 -0700

    refactor: added common elements to Quiz component, moved helper functions to helper file

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 
 import { styles } from './styles'
+import { checkNumber } from './utils'
 
 import { Title, Container, Button } from '../Common'
+
 
 export default class Quiz extends Component {
 
 	render(){
 
 		let {title, questions} = this.props.quiz
-		let cardNumber = questions.length
-		let numberText = !questions.length ? 'No cards' : questions.length > 1 ? 'Cards' : 'Card'
+		let cardNumber = checkNumber(questions.length)
+		let numberText = checkNumber(questions.length, true)
 
 		return(
 			<Container>
