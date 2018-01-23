85ad663f67a4187e8f13080fe85463e55e9e08fb
Daniel Escobar <escobard90@gmail.com>
2018-01-23 14:10:10 -0700

    feat: added map handler to questions array

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -5,16 +5,70 @@ import { styles } from './styles'
 import { Title, Container, Button } from '../Common'
 
 export default class Quiz extends Component {
-	
+
+	constructor(props) {
+		super(props)
+
+		this.state={
+			showAnswer: false,
+			cardNumber: 0,
+			currentCard: 0,
+		}
+
+		this.handleAnswer = this.handleAnswer.bind(this);
+		this.renderCard = this.renderCard.bind(this);
+	}
+
+	renderCard(questions, content, cardNumber, currentCard){
+
+		return questions.map((item, key) => {
+			// checks current card number vs key
+			if (currentCard != key) { return }
+			
+			/*
+			console.log('item', item)	
+			console.log('key', key)
+			console.log('content', content)
+			let {question, answer } = item
+			*/
+			
+			return (
+
+				<Container key={key}>
+					<Title text={content} addStyles={styles.title}/>
+				</Container>
+
+			)
+
+		})
+	}
+
+	handleAnswer(showAnswer){
+
+		this.setState({
+			showAnswer: !showAnswer
+		})
+	}
+
 	render(){
+
 		let { quiz } = this.props
+		let { showAnswer, cardNumber, currentCard } = this.state
 		let { title, questions } = quiz
-		let { answer, question} = questions[0]
+		let { answer, question } = questions[0]
+
+		let content =  showAnswer ? answer : question 
+		let btnContent = showAnswer ? 'Show Question' : 'Show Answer'
 
 		return(
-			<Container>
-				<Title text={answer}/>
-				<Title text={question}/>
+			<Container addStyles={styles.container}>
+				{this.renderCard(questions, content, cardNumber, currentCard)}
+				
+				<Button
+				 text={btnContent} 
+				 handler={() => this.handleAnswer(showAnswer)}
+				/>
+
 				<Button text={'Correct'}/>
 				<Button text={'Incorrect'}/>
 			</Container>
---
+++ src/components/Quiz/styles.js
@@ -0,0 +1,13 @@
+
+import { StyleSheet } from 'react-native'
+
+import { common, space, colors } from '../../styles'
+
+export const styles = StyleSheet.create({
+  title:{ 
+      height: space.xxxlrg + space.xxxlrg
+  },
+  container: {
+      paddingBottom: space.xxlrg
+  }
+});
