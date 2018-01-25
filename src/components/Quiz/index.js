b17eac26b0a86ab23df24fab1a395143da85fce2
Daniel Escobar <escobard90@gmail.com>
2018-01-24 18:08:00 -0700

    refactor: refactored Score component into util - need to refactor card as well when more complete - last commit of v0.6

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,48 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
-import { styles } from './styles'
+import { styles } from "./styles"
 
-import { Title, Container, Button } from '../Common'
+import { Title, Container, Button } from "../Common"
+
+import Score from "../Score"
 
 export default class Quiz extends Component {
-
 	constructor(props) {
 		super(props)
 
-		this.state={
+		this.state = {
 			showAnswer: false,
 			cardNumber: 0,
 			currentCard: 0,
 			showScore: false
 		}
 
-		this.handleAnswer = this.handleAnswer.bind(this);
-		this.renderCards = this.renderCards.bind(this);
-		this.nextCard = this.nextCard.bind(this);
+		this.handleAnswer = this.handleAnswer.bind(this)
+		this.renderCards = this.renderCards.bind(this)
+		this.nextCard = this.nextCard.bind(this)
+		this.renderContent = this.renderContent.bind(this)
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		this.setState({
 			cardNumber: this.props.quiz.questions.length
 		})
-		console.log('MOUNT')
 	}
 
-	renderCards(questions, showAnswer, cardNumber, currentCard, showScore){
-		
-		if (showScore) {
-			return(
-				<Container>
-					<Title text={'Score details go here'} addStyles={styles.title}/>
-				</Container>
-			)
-		}
+	renderCards(questions, showAnswer, cardNumber, currentCard, showScore) {
 
+		// this should be refactored into a separate component in the future
 		return questions.map((item, key) => {
 			// checks current card number vs key
 			// need to add the score screen text here
-			if (currentCard != key) { return }
-			
+			if (currentCard != key) {
+				return
+			}
+
 			/*
 			console.log('item', item)	
 			console.log('key', key)
@@ -51,55 +47,51 @@ export default class Quiz extends Component {
 
 			let { answer, question } = item
 
-			let content =  showAnswer ? answer : question
+			let content = showAnswer ? answer : question
 
 			return (
-
 				<Container key={key}>
-					<Title text={content} addStyles={styles.title}/>
+					<Title text={content} addStyles={styles.title} />
 				</Container>
-
 			)
-
 		})
 	}
 
-	renderButtons(btnContent, showAnswer, cardNumber, currentCard, showScore, goBack){
-		if (showScore) {
-			return (
-
-				<Container>
-					<Button text={'Restart Quiz'} handler={restartQuiz}/>
-					<Button text={'Back to Quiz Details'} handler={goBack}/>
-				</Container>
-			)
-		}
-		// need to add a handler for when the 
+	renderButtons(
+		btnContent,
+		showAnswer,
+		cardNumber,
+		currentCard,
+		showScore,
+		goBack
+	) {
 		return (
 			<Container>
 				<Button
-				 text={btnContent} 
-				 handler={() => this.handleAnswer(showAnswer)}
+					text={btnContent}
+					handler={() => this.handleAnswer(showAnswer)}
 				/>
 
 				<Button
-				 text={'Correct'}
-				 handler={() => this.nextCard(showAnswer, cardNumber, currentCard)}
+					text={"Correct"}
+					handler={() =>
+						this.nextCard(showAnswer, cardNumber, currentCard)
+					}
 				/>
 
 				<Button
-				 text={'Incorrect'}
-				 handler={() => this.nextCard(showAnswer, cardNumber, currentCard)}
+					text={"Incorrect"}
+					handler={() =>
+						this.nextCard(showAnswer, cardNumber, currentCard)
+					}
 				/>
 			</Container>
-
 		)
 	}
 
-	nextCard(showAnswer, cardNumber, currentCard){
-		
+	nextCard(showAnswer, cardNumber, currentCard) {
 		// refactor this into a helper file after, in v0.7 during v0.6 cleanup
-		let showScore = ( cardNumber, currentCard ) => {
+		let showScore = (cardNumber, currentCard) => {
 			if (currentCard == cardNumber - 1) {
 				return true
 			}
@@ -112,27 +104,51 @@ export default class Quiz extends Component {
 			showAnswer: false,
 			showScore: showScore(cardNumber, currentCard)
 		})
-
 	}
 
-	handleAnswer(showAnswer){
-
+	handleAnswer(showAnswer) {
 		this.setState({
 			showAnswer: !showAnswer
 		})
 	}
 
-	render(){
-
-		let { quiz, goBack } = this.props
-		let { showAnswer, cardNumber, currentCard, showScore } = this.state
+	renderContent(showScore) {
+		let { quiz, goBack, restartQuiz } = this.props
+		let { showAnswer, cardNumber, currentCard } = this.state
 		let { title, questions } = quiz
-		let btnContent = showAnswer ? 'Show Question' : 'Show Answer'
+		let btnContent = showAnswer ? "Show Question" : "Show Answer"
 
-		return(
+		if (showScore) {
+			return <Score results={quiz} restartQuiz={restartQuiz} goBack={goBack} />
+		}
+		return (
 			<Container addStyles={styles.container}>
-				{this.renderCards(questions, showAnswer, cardNumber, currentCard, showScore)}
-				{this.renderButtons(btnContent, showAnswer, cardNumber, currentCard, showScore, goBack)}
+				{this.renderCards(
+					questions,
+					showAnswer,
+					cardNumber,
+					currentCard,
+					showScore
+				)}
+				{this.renderButtons(
+					btnContent,
+					showAnswer,
+					cardNumber,
+					currentCard,
+					showScore,
+					goBack,
+					restartQuiz
+				)}
+			</Container>
+		)
+	}
+
+	render() {
+		let { showScore } = this.state
+
+		return (
+			<Container addStyles={styles.container}>
+				{this.renderContent(showScore)}
 			</Container>
 		)
 	}
---
+++ src/components/Score/styles.js
@@ -0,0 +1,10 @@
+
+import { StyleSheet } from 'react-native'
+
+import { space, colors } from '../../styles'
+
+export const styles = StyleSheet.create({
+  title:{ 
+      height: space.xxxlrg + space.xxxlrg
+  }
+});
