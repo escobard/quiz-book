07c40a944743c3e09cca6cd31fafe91bf9f5758a
Daniel Escobar <escobard90@gmail.com>
2018-02-26 16:44:41 -0700

    feat: added conditional handler to display start quiz button depending on card number

--- src/components/QuizDescription/index.js
+++ src/components/QuizDescription/index.js
@@ -5,6 +5,11 @@ import { checkNumber } from "./utils"
 import { Title, Container, Button } from "../Common"
 
 export default class QuizDescription extends Component {
+	startButton(cardsNumber, startQuiz) {
+		if (cardsNumber) {
+			return <Button text={"Start Quiz"} handler={startQuiz} />
+		}
+	}
 	render() {
 		let { quiz: { title, questions }, addCard, startQuiz } = this.props
 		let cardsNumber = questions.length
@@ -12,14 +17,16 @@ export default class QuizDescription extends Component {
 		let cardNumber = checkNumber(cardsNumber)
 		let numberText = checkNumber(cardsNumber, true)
 
-		let subText = cardsNumber ? `${cardNumber} ${numberText}` : "No cards"
+		let subText = cardsNumber
+			? `${cardNumber} ${numberText}`
+			: "Add a card to get started"
 
 		return (
 			<Container addStyles={styles.centered}>
 				<Title text={title} />
 				<Title isSubtitle={true} text={subText} />
 				<Button text={"Add Card"} handler={addCard} />
-				<Button text={"Start Quiz"} handler={startQuiz} />
+				{this.startButton(cardsNumber, startQuiz)}
 			</Container>
 		)
 	}
